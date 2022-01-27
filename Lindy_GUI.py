fc3ba930eaee6727908db87d27a40f0d692f1b62
Oleg Budaev <t0r1n88@mail.ru>
2022-01-27 11:33:41 +0800

    Добавлен переключатель для изменения способа обработки данных для индивидуальных и списочных документов

--- Lindy_GUI.py
+++ Lindy_GUI.py
@@ -62,6 +62,7 @@ def generate_docs_dpo():
             context = row
             # Превращаем строку в список кортежей, где первый элемент кортежа это ключ а второй данные
             id_row = list(row.items())
+
             try:
                 doc.render(context)
 
@@ -76,14 +77,17 @@ def generate_docs_dpo():
 
 def generate_docs_po():
     """
-    Функция для создания договоров
+    Функция для создания документов ПО
     :return:
     """
     try:
-        # Считываем csv файл, не забывая что екселевский csv разделен на самомо деле не запятыми а точкой с запятой
-        reader = csv.DictReader(open(name_file_data_doc), delimiter=';')
-        # Конвертируем объект reader в список словарей
-        data = list(reader)
+        # Считываем данные с листа ДПО в указанной таблице
+        df = pd.read_excel(name_file_data_doc,sheet_name='ПО')
+
+        # Конвертируем датафрейм в список словарей
+        data = df.to_dict('records')
+
+
         # Создаем в цикле документы
         for row in data:
             doc = DocxTemplate(name_file_template_doc)
@@ -92,20 +96,51 @@ def generate_docs_po():
             id_row = list(row.items())
             try:
                 doc.render(context)
-                print(context)
-                print(id_row[0][1])
+
                 doc.save(f'{path_to_end_folder_doc}/{id_row[0][1]}.docx')
             except:
-                messagebox.showerror('Dodger','Проверьте содержимое шаблона\nНе допускаются пробелы в словах внутри фигурных скобок')
+                messagebox.showerror('ЦОПП Бурятия','Проверьте содержимое шаблона\nНе допускаются любые символы кроме _ в словах внутри фигурных скобок\nСлова должны могут быть разделены нижним подчеркиванием')
                 continue
-        messagebox.showinfo('Dodger', 'Создание документов успешно завершено!')
+        messagebox.showinfo('ЦОПП Бурятия', 'Создание документов успешно завершено!')
     except NameError as e:
-        messagebox.showinfo('Dodger', f'Выберите шаблон,файл с данными и папку куда будут генерироваться файлы')
+        messagebox.showinfo('ЦОПП Бурятия', f'Выберите шаблон,файл с данными и папку куда будут генерироваться файлы')
+
+def generate_docs_other():
+    """
+    Функция для создания документов из произвольных таблиц(т.е. отличающихся от структуры базы данных ЦОПП Бурятия)
+    :return:
+    """
+    try:
+        # Считываем данные
+        df = pd.read_excel(name_file_data_doc)
+
+        # Конвертируем датафрейм в список словарей
+        data = df.to_dict('records')
+
+
+        # Создаем в цикле документы
+        for row in data:
+            doc = DocxTemplate(name_file_template_doc)
+            context = row
+            # Превращаем строку в список кортежей, где первый элемент кортежа это ключ а второй данные
+            id_row = list(row.items())
+            try:
+                doc.render(context)
+
+
+                doc.save(f'{path_to_end_folder_doc}/{id_row[0][1]}.docx')
+            except:
+                messagebox.showerror('ЦОПП Бурятия','Проверьте содержимое шаблона\nНе допускаются любые символы кроме _ в словах внутри фигурных скобок\nСлова должны могут быть разделены нижним подчеркиванием')
+                continue
+        messagebox.showinfo('ЦОПП Бурятия', 'Создание документов успешно завершено!')
+    except NameError as e:
+        messagebox.showinfo('ЦОПП Бурятия', f'Выберите шаблон,файл с данными и папку куда будут генерироваться файлы')
+
 
 if __name__ == '__main__':
     window = Tk()
     window.title('ЦОПП Бурятия')
-    window.geometry('500x800')
+    window.geometry('650x860')
     window.resizable(False,False)
 
     # path_to_icon = resource_path('favicon.ico')
@@ -125,18 +160,29 @@ if __name__ == '__main__':
     lbl_hello = Label(tab_create_doc, text='Центр опережающей профессиональной подготовки Республики Бурятия\nГенерация документов по шаблону')
     lbl_hello.grid(column=0, row=0, padx=10, pady=25)
 
+    # Переключатель:индивидуальный или списочный приказл
+    # Создаем переменную хранящую тип документа, в зависимости от значения будет использоваться та или иная функция
+    group_rb_type_doc = IntVar()
+
+    frame_rb_type_doc = LabelFrame(tab_create_doc, text='Выберите тип создаваемого документа')
+    frame_rb_type_doc.grid(column=0, row=1, padx=10)
+
+    Radiobutton(frame_rb_type_doc, text='Индивидуальные документы', variable=group_rb_type_doc, value=0).pack()
+    Radiobutton(frame_rb_type_doc, text='Списочный документ', variable=group_rb_type_doc, value=1).pack()
+
+
     #Картинка
     path_to_img = resource_path('logo.png')
     img = PhotoImage(file=path_to_img)
     Label(tab_create_doc,
         image=img
-    ).grid(column=0, row=1, padx=10, pady=25)
+    ).grid(column=0, row=2, padx=10, pady=25)
 
     # Создаем кнопку Выбрать шаблон
     btn_template_contract = Button(tab_create_doc, text='1) Выберите шаблон документа', font=('Arial Bold', 20),
                                    command=select_file_template_doc
                                    )
-    btn_template_contract.grid(column=0, row=2, padx=10, pady=10)
+    btn_template_contract.grid(column=0, row=3, padx=10, pady=10)
 
 
 
@@ -144,27 +190,35 @@ if __name__ == '__main__':
     btn_data_contract = Button(tab_create_doc, text='2) Выберите файл с данными', font=('Arial Bold', 20),
                                command=select_file_data_doc
                                )
-    btn_data_contract.grid(column=0, row=3, padx=10, pady=10)
+    btn_data_contract.grid(column=0, row=4, padx=10, pady=10)
 
     # Создаем кнопку для выбора папки куда будут генерироваться файлы
 
     btn_choose_end_folder_contract = Button(tab_create_doc, text='3) Выберите конечную папку', font=('Arial Bold', 20),
                                             command=select_end_folder_doc
                                             )
-    btn_choose_end_folder_contract.grid(column=0, row=4, padx=10, pady=10)
+    btn_choose_end_folder_contract.grid(column=0, row=5, padx=10, pady=10)
 
     # Создаем кнопку для запуска функции генерации файлов ДПО
 
     btn_create_files_dpo = Button(tab_create_doc, text='Создать документы ДПО', font=('Arial Bold', 20),
                                        command=generate_docs_dpo
                                        )
-    btn_create_files_dpo.grid(column=0, row=5, padx=10, pady=10)
+    btn_create_files_dpo.grid(column=0, row=6, padx=10, pady=10)
 
     # Создаем кнопку для запуска функции генерации файлов ПО
     btn_create_files_po = Button(tab_create_doc, text='Создать документы ПО', font=('Arial Bold', 20),
                                        command=generate_docs_po
                                        )
-    btn_create_files_po.grid(column=0, row=6, padx=10, pady=10)
+    btn_create_files_po.grid(column=0, row=7, padx=10, pady=10)
+
+    # Создаем кнопку для создания документов из таблиц с произвольной структурой
+    btn_create_files_other = Button(tab_create_doc,text='Создать документы из произвольной таблицы',font=('Arial Bold', 20),
+                                       command=generate_docs_other
+                                       )
+    btn_create_files_other.grid(column=0,row=8,padx=10,pady=10)
+
+
 
 
 
--- lindy.py
+++ lindy.py
@@ -2,22 +2,29 @@ import pandas as pd
 from docxtpl import DocxTemplate
 
 
-df = pd.read_excel('data/Тестовая база ДПО.xlsx')
+
+
+
+df = pd.read_excel('data/Форма базы данных.xlsx')
 print(df.head())
 
 
 data = df.to_dict('records')
-print(data)
+
+lst_students = []
 
 for row in data:
-    doc = DocxTemplate('data/Короткий тестовый шаблон ДПО.docx')
+    doc = DocxTemplate('data/Форма Приказ о зачислении_ДПО.docx')
     context = row
+    fio = row['ФИО_именительный']
+    lst_students.append(fio)
+
     # Превращаем строку в список кортежей, где первый элемент кортежа это ключ а второй данные
-    id_row = list(row.items())
+    # id_row = list(row.items())
 
-
-    print(id_row[0][1])
     doc.render(context)
-    doc.save(f'{id_row[0][1]}.docx')
+    doc.save(f'{fio}.docx')
+
+doc = DocxTemplate('data/Форма Приказ о зачислении_ДПО.docx')
 
 
